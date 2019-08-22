156bfa6b1ea4e318aba8c0f5d98a5b51a2526e57
Pluto She <plutoshe@gmail.com>
2019-08-22 09:54:15 -0600

    fix minor bugs

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : Singleton<T>
+public class DataManager : Singleton<DataManager>
 {
     // Start is called before the first frame update
     void Start()
