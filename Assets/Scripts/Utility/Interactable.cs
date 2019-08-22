4d7889db7ba4da10c32f787bea9fc4dac9814bef
Luis Garcia Remes <luisagarciaremes@hotmail.com>
2019-08-22 12:00:55 -0600

    Added Interactable and Whitebox

---
+++ Assets/Scripts/Utility/Interactable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private bool m_interactable = true;
+    public virtual void Interact()
+    {
+        
+    }
+
+    public bool CanInteract()
+    {
+        return m_interactable;
+    }
+
+    public void SetInteract(bool set)
+    {
+        m_interactable = set;
+    }
+}
