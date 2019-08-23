912ef3a1d44afdea033ee81b22b66897e8db4cb4
zzzemma <42650473+zzzemma@users.noreply.github.com>
2019-08-22 21:42:04 -0600

    Update IdentityManager.cs

--- Assets/Scripts/IdentityManager.cs
+++ Assets/Scripts/IdentityManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IdentityManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Image Identity01;
+    public Image Identity02;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
