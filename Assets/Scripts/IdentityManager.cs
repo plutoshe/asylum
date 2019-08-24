393ace67338087edffbb48b379434285b08807c0
zzzemma <42650473+zzzemma@users.noreply.github.com>
2019-08-24 14:14:37 -0600

    mirror function finished

--- Assets/Scripts/IdentityManager.cs
+++ Assets/Scripts/IdentityManager.cs
@@ -7,16 +7,57 @@ public class IdentityManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Image Identity01;
+    public Animator FemaleGlow;
+    public Animator FemaleShow;
     public Image Identity02;
+    public Animator MaleShow;
 
+    public Text GameText;
+    private int IdentityNum;
     void Start()
     {
+        FemaleGlow.gameObject.SetActive(false);
 
+        FemaleGlow.SetBool("GlowStart", false);
+        FemaleShow.SetBool("Show", false);
+        MaleShow.SetBool("Show", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            FemaleGlow.gameObject.SetActive(true);
+
+            FemaleGlow.SetBool("GlowStart", true);
+            FemaleShow.SetBool("Show", true);
+            MaleShow.SetBool("Show", false);
+            IdentityNum = 1;
+
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            FemaleGlow.gameObject.SetActive(false);
+
+            FemaleGlow.SetBool("GlowStart", false);
+            FemaleShow.SetBool("Show", false);
+            MaleShow.SetBool("Show", true);
+
+            IdentityNum = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(IdentityNum == 1)
+            {
+                GameText.text = "Female Character has been selected.";
+            }
+            else if(IdentityNum == 2)
+            {
+                GameText.text = "Male Character has been selected.";
+            }
+
+
+        }
     }
 }
