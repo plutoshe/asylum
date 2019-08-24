e4bfd9e3519bee29d928027d63d24a0b821cbacc
LuisGarciaRemes <luisagarciaremes@hotmail.com>
2019-08-24 11:40:12 -0600

    Refined interact script. Added visual helpers

--- Assets/Scripts/Player/InteractController.cs
+++ Assets/Scripts/Player/InteractController.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractController : MonoBehaviour
 {
+
+    public Image m_reticle;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.GetComponent<Interactable>() != null)
         {
+            m_reticle.GetComponent<RectTransform>().sizeDelta = new Vector2(100, 100);
             if (Input.GetMouseButtonDown(0) && other.gameObject.GetComponent<Interactable>().CanInteract())
             {
                 other.gameObject.GetComponent<Interactable>().Interact();
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<Interactable>() != null)
+        {
+            m_reticle.GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50);
         }
     }
 }
