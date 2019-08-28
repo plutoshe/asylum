e0e118afc817d76b562b040dd2ca8a52ad10fd42
zzzemma <42650473+zzzemma@users.noreply.github.com>
2019-08-28 14:52:55 -0600

    add Hide in bed scene

---
+++ Assets/Scripts/HideFuntion.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideFuntion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject m_MainCam;
+    public GameObject m_HideCam;
+    public Animator m_MonsterMove;
+
+
+    void Start()
+    {
+        m_MainCam.SetActive(true);
+        m_HideCam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            m_MainCam.SetActive(false);
+            m_HideCam.SetActive(true);
+            m_MonsterMove.SetBool("MonsterMoveStart", true);
+            
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            m_MainCam.SetActive(true);
+            m_HideCam.SetActive(false);
+            m_MonsterMove.SetBool("MonsterMoveStart", false);
+        }
+    }
+
+  
+}
---
+++ Assets/Scripts/HideTextAndNoise.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HideTextAndNoise : MonoBehaviour
+{
+    public Text m_hint;
+    public Animator m_NoiseScreen;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ShowText()
+    {
+        m_hint.gameObject.SetActive(true);
+    }
+    public void HideText()
+    {
+        m_hint.gameObject.SetActive(false);
+    }
+
+    public void ShowNoiseScreen()
+    {
+        m_NoiseScreen.gameObject.SetActive(true);
+        m_NoiseScreen.SetBool("NoiseScreenStart", true);
+    }
+
+    public void StopNoiseScreen()
+    {
+        m_NoiseScreen.SetBool("NoiseScreenStop", true);
+    }
+}
