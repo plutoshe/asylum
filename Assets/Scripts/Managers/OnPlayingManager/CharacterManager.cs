85d6188a93a6aed71a45ad589f01666bf5ea5002
Pluto She <plutoshe@gmail.com>
2019-08-27 20:45:46 -0600

    add character daynight change

---
+++ Assets/Scripts/Managers/OnPlayingManager/CharacterManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    private List<Transform> m_dailyTimeCharacters;
+    private List<Transform> m_nightCharacters;
+
+    public bool HasString(string origin, string match)
+    {
+        return match.Contains(origin);
+    }
+
+    private void OnEnable()
+    {
+        bool isDayTime = DataManager.Instance.IsDayTime();
+        if (isDayTime)
+        {
+            SetDayTime();
+        } else
+        {
+            SetNight();
+        }
+        CustomEventManager.Instance.StartListening(CustomEventConstant.s_ToDayTime, SetDayTime);
+        CustomEventManager.Instance.StartListening(CustomEventConstant.s_ToNight, SetNight);
+    }
+
+    private void Awake()
+    {
+        m_dailyTimeCharacters = transform.FindDeepChildren("Character", HasString);
+        m_nightCharacters = transform.FindDeepChildren("Monster", HasString);
+    }
+
+    public void SetDayTime()
+    {
+        for (int i = 0; i < m_dailyTimeCharacters.Count; i++)
+        {
+            m_dailyTimeCharacters[i].gameObject.SetActive(true);
+        }
+        for (int i = 0; i < m_nightCharacters.Count; i++)
+        {
+            m_nightCharacters[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void SetNight()
+    {
+        for (int i = 0; i < m_dailyTimeCharacters.Count; i++)
+        {
+            m_dailyTimeCharacters[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < m_nightCharacters.Count; i++)
+        {
+            m_nightCharacters[i].gameObject.SetActive(true);
+        }
+    }
+
+}
--- Assets/Scripts/Utility/Class/TransformExtension.cs
+++ Assets/Scripts/Utility/Class/TransformExtension.cs
@@ -34,4 +34,33 @@ public static class TransformDeepChildExtension
             return null;
         }
     }
+
+
+    public delegate bool MatchPattern(string origin, string match);
+
+    public static bool ExactMatch(string origin, string match)
+    {
+        return origin == match;
+    }
+
+
+    public static List<Transform> FindDeepChildren(this Transform aParent, string aName)
+    {
+        return FindDeepChildren(aParent, aName, ExactMatch);
+    }
+
+    public static List<Transform> FindDeepChildren(this Transform aParent, string aName, MatchPattern matchFunction)
+    {
+        List<Transform> children = new List<Transform>();
+        foreach (Transform child in aParent)
+        {
+            if (matchFunction(aName, child.name))
+            {
+                children.Add(child);
+            }
+            List<Transform> result = child.FindDeepChildren(aName, matchFunction);
+            children.AddRange(result);
+        }
+        return children;
+    }
 }
