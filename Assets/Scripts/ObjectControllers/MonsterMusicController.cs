c1409a837b25886021f86ef314d307b4bbcc1cb0
zzzemma <42650473+zzzemma@users.noreply.github.com>
2019-08-29 02:03:01 -0600

    music change

---
+++ Assets/Scripts/ObjectControllers/MonsterMusicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMusicController : MonoBehaviour
+{
+    [SerializeField] AudioClip m_exitClip;
+    [SerializeField] AudioClip m_enterClip;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MusicManager.Instance.ChangeBGMusic(m_enterClip);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MusicManager.Instance.ChangeBGMusic(m_exitClip);
+        }     
+    }
+}
