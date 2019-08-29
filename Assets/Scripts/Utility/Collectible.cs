1350a4f95209436329d0f458ca0e04da4d3f3d14
Pluto She <plutoshe@gmail.com>
2019-08-28 23:54:20 -0600

    polish dialog and item

--- Assets/Scripts/Utility/Collectible.cs
+++ Assets/Scripts/Utility/Collectible.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Collectible : Interactable
 {
     [SerializeField] private Sprite m_inventoryImage = null;
+    [SerializeField] bool m_rescale = false;
+    [SerializeField] Vector3 m_newScale = new Vector3(0,0,0);
     private int m_slot = 5;
 
     public override void Interact()
     {
+        if(m_rescale)
+        {
+            this.gameObject.transform.localScale = m_newScale;
+        }
         Inventory.Instance.PlaceItemInInventory(this.gameObject);
     }
 
